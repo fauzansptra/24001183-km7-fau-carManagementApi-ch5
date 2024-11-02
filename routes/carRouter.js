368dcd64c5177e3deb07502c3adb7dafcfb0f41f
xkylz1 <fauzansaputra741@gmail.com>
2024-11-03 06:03:39 +0800

    feat: add deleted car list

--- routes/carRouter.js
+++ routes/carRouter.js
@@ -5,7 +5,17 @@ const authenticate = require("../middlewares/authenticate");
 
 router.post("", authenticate(["admin", "superadmin"]), carController.createCar);
 router.get("", carController.getAllCar);
-router.get("/:id", carController.getCarById);
+router.get(
+  "/soft-deleted",
+  authenticate(["admin", "superadmin"]),
+  carController.getDeletedCars
+);
+router.get(
+  "/:id",
+  authenticate(["admin", "superadmin"]),
+  carController.getCarById
+);
+
 router.patch(
   "/:id",
   authenticate(["admin", "superadmin"]),
